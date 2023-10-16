f77c91d7c3489fd9ec000991d4a51b929c6b4d7a
unknown <teteulima@outlook.com>
2023-10-15 22:20:11 -0300

    init the javascript

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// f23e435017d6ee279843c3b207b5f0b6
+//variables
+const apiKey =  "f23e435017d6ee279843c3b207b5f0b6"
+const apiCountry = "https://flagsapi.com//flat/64.png"
+
+const cityInput = document.querySelector("#city-input")
+const searchBtn = document.querySelector("#search")
+
+const cityElement = document.querySelector("#namecity")
+const cityElement = document.querySelector("#namecity")
+const cityElement = document.querySelector("#namecity")
+const cityElement = document.querySelector("#namecity")
+const cityElement = document.querySelector("#namecity")
+const cityElement = document.querySelector("#namecity")
+const cityElement = document.querySelector("#namecity")
+
+//functions
+const dataWeather = (city) =>{
+    console.log(city)
+}
+
+//events 
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault()
+
+    const city = cityInput.value;
+
+    dataWeather(city)
+})
